d69f7c1952c0b183d86e41498446ccbd62446dfc
JavMatz <erick97am@gmail.com>
2022-03-04 18:22:46 -0600

    Changed colorscheme

--- config.h
+++ config.h
@@ -12,31 +12,18 @@ static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = {"monospace:size=11"};
 static const char dmenufont[]       = "monospace:size=11";
 
-static const char normbgcolor[]       = "#000000";
-static const char normbordercolor[]   = "#2c001e";
+static const char normbgcolor[]       = "#222222";
+static const char normbordercolor[]   = "#444444";
 static const char normfgcolor[]       = "#bbbbbb";
 static const char selfgcolor[]        = "#eeeeee";
-static const char selbordercolor[]    = "#aea79f";
-static const char selbgcolor[]        = "#e95420";
+static const char selbordercolor[]    = "#770000";
+static const char selbgcolor[]        = "#005577";
 
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { normfgcolor, normbgcolor, normbordercolor },
 	[SchemeSel]  = { selfgcolor, selbgcolor,  selbordercolor  },
 };
-
-/* static const char *fonts[]          = { "monospace:size=10" }; */
-/* static const char dmenufont[]       = "monospace:size=10"; */
-/* static const char col_gray1[]       = "#222222"; */
-/* static const char col_gray2[]       = "#444444"; */
-/* static const char col_gray3[]       = "#bbbbbb"; */
-/* static const char col_gray4[]       = "#eeeeee"; */
-/* static const char col_cyan[]        = "#005577"; */
-/* static const char *colors[][3]      = { */
-/* 	/\*               fg         bg         border   *\/ */
-/* 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 }, */
-/* 	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  }, */
-/* }; */
 
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
